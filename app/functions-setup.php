1b4c0eb76aec2558704a2a7e0db4198a526222eb
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-04 08:00:17 -0700

    initial commit

---
+++ app/functions-setup.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default theme setup.
+ *
+ * This is where all the add_theme_support(); will happen.
+ *
+ * @package   Generosity
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/generosity
+ */
+
+namespace Generosity;
+
+/**
+ * Set up theme support.  This is where calls to `add_theme_support()` happen.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+add_action( 'after_setup_theme', function() {
+
+	// Automatically add the `<title>` tag.
+	add_theme_support( 'title-tag' );
+
+	// Automatically add feed links to `<head>`.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Outputs HTML5 markup for core features.
+	add_theme_support( 'html5', [ 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ] );
+} );
