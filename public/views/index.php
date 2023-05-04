abbadc2e01da77128e1c422b5bfb98173b44cf5b
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-04 07:34:08 -0700

    initial commit

---
+++ public/views/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Default index template
+ *
+ * @package   Generosity
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/benjlu.com
+ */
+
+// Loads header/*.php templates
+Backdrop\Template\View\display( 'header', Backdrop\Template\Hierarchy\hierarchy() );
+
+// Loads content/*.php templates
+Backdrop\Template\View\display( 'content', Backdrop\Template\Hierarchy\hierarchy() );
+
+// Loads footer/*.php templates
+Backdrop\Template\View\display( 'footer', Backdrop\Template\Hierarchy\hierarchy() );
