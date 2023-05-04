00c7aaffbc9a39193563a6643388f16f708a8544
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-04 16:24:11 -0700

    updated

--- public/views/entry/404.php
+++ public/views/entry/404.php
@@ -1,11 +1,11 @@
 <article class="post-0">
 
 	<div class="entry-header">
-		<h1 class="entry-title"><?php esc_html_e( 'Whoah, partner!' ) ?></h1>
+		<h1 class="entry-title"><?php esc_html_e( 'Whoah, partner!', 'generosity' ) ?></h1>
 	</div>
 	<div class="entry_content">
 		<p>
-			<?php esc_html_e( 'It looks like you stumbled upon a page that does not exist. Perhaps rolling the dice with a search might help.' ) ?>
+			<?php esc_html_e( 'It looks like you stumbled upon a page that does not exist. Perhaps rolling the dice with a search might help.', 'generosity' ) ?>
 		</p>
 		<?php get_search_form() ?>
 	</div>
